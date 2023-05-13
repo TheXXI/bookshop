bdce4c3ec6c8f87e6057c667795485ef5686a105
Mikhail <mk_111@bk.ru>
2023-05-13 11:09:52 +0300

    desktop layout final

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 import './scss/base.scss';
 
 import './js/categories.js';
+import './js/slider.js';
---
+++ src/js/slider.js
@@ -0,0 +1,15 @@
+/*const imagesData = [
+    { 'img': '../../src/images/banner1.svg' },
+    { 'img': '../../src/images/banner2.svg' },
+    { 'img': '../../src/images/banner3.svg' }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const pointsElements = document.querySelector('.slider-points').childNodes;
+    const imageBlock = document.getElementById('slider-image');
+    pointsElements.forEach(item => {
+        item.addEventListener('click', {
+        
+        });
+    })
+});*/
