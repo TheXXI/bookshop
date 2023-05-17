19fd9bc3b653b8a6de1ce110ba5656257a35e85a
Mikhail <mk_111@bk.ru>
2023-05-17 18:53:48 +0300

    edit card.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,33 +1,70 @@
-const addCardButtons = document.querySelectorAll('#add-remove-cart');
+import { replaceAllInString } from './tools.js';
 const cartHeader = document.getElementById('books-in-cart');
 
 export function initCartButtons() {
-    console.log(addCardButtons);
-    console.log('localStorage.length: ' + localStorage.length);
+    const addCardButtons = document.querySelectorAll('#add-remove-cart');
+    console.log(localStorage);
 
-    if (localStorage.length == 0) localStorage.setItem('booksInCart', []);
+    if (localStorage.length == 0) localStorage.setItem('booksInCart', '');
 
     addCardButtons.forEach(button => {
+        const currentBookId = button.closest('.book-item').dataset.index;
+        let checkBookInCard = false;
+        let booksInCart = localStorage.getItem('booksInCart');
+        if (booksInCart != '') {
+            booksInCart.split(',').forEach((element, index) => {
+                if (element == currentBookId) checkBookInCard = true;
+
+            });
+
+            if (checkBookInCard) {
+                button.classList.add('remove-cart-button');
+                button.textContent = 'in the cart';
+            }
+        }
+
         button.addEventListener('click', (e) => {
+
             const currentBookId = e.target.closest('.book-item').dataset.index;
 
             let checkBookInCard = false;
-            const booksInCart = localStorage.getItem('booksInCart');
-            for (const bookId in booksInCart.split(',')) {
-                console.log (bookId + ' : ' + currentBookId);
-                if (bookId == currentBookId) {
-                    checkBookInCard = true;
-                    break;
-                }
-            }
+            let booksInCart = localStorage.getItem('booksInCart');
+            booksInCart.split(',').forEach((element, index) => {
+                if (element == currentBookId) checkBookInCard = true;
+            });
 
             if (checkBookInCard) {
-                e.target.classList.add('btn');
-            } 
-            //let booksInCart = localStorage.getItem('booksInCart');
-            //if (booksInCart == '') booksInCart = bookId;
-            //else booksInCart += ',' + bookId;
-            //localStorage.setItem('booksInCart', booksInCart);
+
+                console.log('---------');
+                console.log('  ' + typeof(booksInCart));
+                console.log('  >' + booksInCart + '<');
+                booksInCart = booksInCart.replace('X8KlDwAAQBAJ', '');
+                console.log('  >' + booksInCart + '<');
+                localStorage.setItem('booksInCart', booksInCart);
+                console.log('---------');
+
+                e.target.classList.remove('remove-cart-button');
+                button.textContent = 'buy now';
+
+                reloadCartHeader();
+
+                console.log('remove');
+                console.log(localStorage);
+            } else {
+                if (booksInCart == '') booksInCart = currentBookId;
+                else booksInCart += ',' + currentBookId;
+                localStorage.setItem('booksInCart', booksInCart);
+
+                e.target.classList.add('remove-cart-button');
+                button.textContent = 'in the cart';
+
+                reloadCartHeader();
+
+                console.log('add');
+                console.log(localStorage);
+            }
+
+
         });
     });
 }
